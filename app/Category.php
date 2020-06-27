e862df5d4bea06f78cb50e366cb592ffe8f5e64c
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-06-27 15:31:07 -0400

    feat(db): add one-to-many relantion

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['title', 'description'];
+
+
+    /**
+     * Get products.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'sku', 'price'];
+
+
+    /**
+     * Get category.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
