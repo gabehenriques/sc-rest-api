7b15d7cdd070ab815650497a53a71f9dc39ab730
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-06-27 17:11:14 -0400

    tests: remove api tests

--- tests/api/ProductResourceApiCest.php
+++ tests/api/ProductResourceApiCest.php
@@ -7,17 +7,6 @@ class ProductResourceApiCest
 
     public function getAllProducts(ApiTester $I)
     {
-        // $id = (string) $this->havePost($I, ['title' => 'Game of Thrones']);
-        // $id2 = (string) $this->havePost($I, ['title' => 'Lord of the Rings']);
-        $id = $I->haveRecord('products', $this->getPostAttributes(['name' => 'Sik Sik Wat']));
-        $id2 = $I->haveRecord('products', $this->getPostAttributes(['name' => 'Huo Guo']));
-        $I->sendGET($this->endpoint);
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseIsJson();
-        $I->expect('both items are in response');
-        $I->seeResponseContainsJson(['id' => "$id", 'name' => 'Sik Sik Wat']);
-        $I->seeResponseContainsJson(['id' => "$id2", 'name' => 'Huo Guo']);
-        $I->seeResponseContainsJson([['id' => $id], ['id' => $id2]]);
     }
 
 
