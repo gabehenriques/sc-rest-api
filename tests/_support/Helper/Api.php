a767d48068225f712f9b7d826543a8f8c03ae9a5
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-07-02 18:09:07 -0400

    tests(api): add first tests around GET HTTP calls

--- tests/_support/Helper/Api.php
+++ tests/_support/Helper/Api.php
@@ -1,10 +1,21 @@
 <?php
 namespace Helper;
 
+use App\Product;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class Api extends \Codeception\Module
 {
+    public function getProducts()
+    {
+        return Product::all()->toArray();
+    }
 
+
+    public function getSingleProducts($id)
+    {
+        return Product::find($id)->toArray();
+    }
 }
--- tests/api/ProductResourceApiCest.php
+++ tests/api/ProductResourceApiCest.php
@@ -1,23 +1,58 @@
 <?php
 
+use App\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 class ProductResourceApiCest
 {
     protected $endpoint = '/api/products';
 
-
     public function getAllProducts(ApiTester $I)
     {
-        //Had no time to put this together....
+        $parm = '';
+
+        $allProducts = $I->getProducts();
+        $I->sendGET($this->endpoint . "/" . $parm);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson($allProducts);
+        $I->seeResponseIsJson();
     }
 
-
-
-    public function _before(ApiTester $I)
+    public function getSingleProducts(ApiTester $I)
     {
+        $id = 1;
+        $singleProduct = $I->getSingleProducts($id);
+
+        $I->sendGET($this->endpoint . "/" . $id);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson($singleProduct);
+        $I->seeResponseIsJson();
     }
 
-    // tests
-    public function tryToTest(ApiTester $I)
+    private function haveProduct(ApiTester $I, $data=[])
     {
+        $data = array_merge([
+        'name' => 'Example Product 1',
+        'category_id' => 1,
+        'sku' => 'FCCDEF12345',
+        'price' => 99.99,
+      ], $data);
+
+        return $I->haveRecord('products', $data);
+    }
+
+    public function getProductDataTypes(ApiTester $I)
+    {
+        $I->sendGET($this->endpoint);
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType([
+            'id' => 'integer',
+            'name' => 'string',
+            'sku' => 'string',
+            'price' => 'float',
+            'category_id' => 'integer',
+       ]);
     }
 }
